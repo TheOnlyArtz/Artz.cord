13d642788db4f47c4057ba495e4e417b9cf20bcb
TheOnlyArtz <gamesil456@gmail.com>
2017-10-17 19:15:44 +0300

    Channel caching algorithm

--- Models/ChannelCaching.js
+++ Models/ChannelCaching.js
@@ -2,7 +2,58 @@
   Cache channels preventing API spam
 */
 class ChannelCaching {
-  constructor(iterable) {
-
+  constructor(client, iterable) {
+    this.iterable = iterable;
+    this.client = client;
   }
+
+  _cache() {
+    let that = this;
+    this.iterable.forEach(i => {
+      i.channels.forEach(o => {
+        that.client.channels.set(o.id, that._cachingObj(o));
+      });
+    });
+  };
+
+  _cachingObj(data) {
+
+    let type = data.type
+    switch (type) {
+      case 0:
+        type = "Text Channel";
+        break;
+      case 1:
+        type = "Direct Messages";
+        break;
+      case 2:
+        type = "Voice Channel";
+        break;
+      case 3:
+        type = "DM Group";
+        break;
+      case 4:
+        type = "Guild Category";
+        break;
+      default: "unknown";
+
+    }
+
+    let initObj = {
+      type                 : type,
+      topic                : data.topic,
+      position             : data.position,
+      permissionOverwrites : data.permission_overwrites,
+      parentChannelID      : data.parent_id,
+      nsfw                 : data.nsfw,
+      name                 : data.name,
+      lastMessageID        : data.last_message_id,
+      id                   : data.id
+    }
+
+    return initObj
+  }
+
 }
+
+module.exports = ChannelCaching;
