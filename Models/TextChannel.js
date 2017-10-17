5e4d23332dd3ce18e52f9ee91331bca2087f04be
TheOnlyArtz <gamesil456@gmail.com>
2017-10-17 20:08:58 +0300

    Working on send function

--- Models/TextChannel.js
+++ Models/TextChannel.js
@@ -1,5 +1,29 @@
+const snekfetch = require('snekfetch')
 class TextChannel {
   constructor(client, data) {
-    
+    this.type = 'text'
   }
+
+  get name() {
+    if (data.name) {
+      return data.name;
+    } else {
+      return 'Unknown';
+    };
+  };
+
+  send(toSend, options) {
+    return new Promise(async function(resolve, reject) {
+    let content;
+
+    if (!toSend) throw Error('Can\'t send an empty message');
+    if (!options.tts) options.tts = false;
+    if (!options.markup) content = toSend;
+
+    if (typeof options.markup !== 'undefined' && (typeof options.markup !== 'boolean' || options.markup === true)) {
+
+      }
+
+    }
+  });
 }
