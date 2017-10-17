b1f3ebafc6543c95b195dae0d1d44e2923aeecf3
TheOnlyArtz <gamesil456@gmail.com>
2017-10-17 18:36:49 +0300

    "this" instead of "data" fix

--- Models/Guild.js
+++ Models/Guild.js
@@ -8,11 +8,11 @@ class Guild {
     this.region = data.region;
     this.afkChannelID = data.afk_channel_id;
     this.afkTimeout = data.afk_timeout;
-    this.embedEnabled = this.embed_enabled;
+    this.embedEnabled = data.embed_enabled;
     this.verificationLevel = data.verification_level;
     this.defaultMessageNotifications = data.default_message_notifications;
-    tgis.embedChannelID = data.embed_channel_id;
-    this.explicitContentFilter = this.explicit_content_filter;
+    this.embedChannelID = data.embed_channel_id;
+    this.explicitContentFilter = data.explicit_content_filter;
     this.roles = data.roles // TODO: Collection
     this.emojis = data.emojis // TODO: Collection
     this.features = data.features;
@@ -25,7 +25,7 @@ class Guild {
     this.members = data.members // TODO: collection
     this.channels = data.channels // TODO: collection
     this.presence = data.presence // TODO: Collection
-  }
+  };
 }
 
 module.exports = Guild;
