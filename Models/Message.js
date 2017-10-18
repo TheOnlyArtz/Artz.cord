66e44e09e65a12f5179a32b139a6b9e90c744972
TheOnlyArtz <gamesil456@gmail.com>
2017-10-18 14:45:29 +0300

    Changed formatting

--- Models/Message.js
+++ Models/Message.js
@@ -1,18 +1,21 @@
+const path = require('path')
 const Guild = require(path.join(__dirname, '..', 'Models', 'Guild.js'));
+const GuildChannel = require(path.join(__dirname, '..', 'Models', 'GuildChannel.js'));
 module.exports = class Message {
   constructor(client, data) {
-    this.tts              = data.tts || null;
-    this.createdTimestamp = data.timestamp || null;
-    this.pinned           = data.pinned || null;
-    this.nonce            = data.nonce || null;
+    this.tts              = data.tts;
+    this.createdTimestamp = data.timestamp;
+    this.pinned           = data.pinned;
+    this.nonce            = data.nonce;
     this.content          = data.content;
-    this.id               = data.id || null;
-    this.channelID        = data.channel_id || null // TODO: Make it new TextChannel
-    this.author           = data.author || null
+    this.id               = data.id;
+    this.channelID        = data.channel_id; // TODO: Make it new TextChannel
+    this.author           = data.author;
     this.mentions         = data.mentions || [];
     this.roleMentions     = data.roleMentions || [];
-    this.attachments      = data.attachments;
-    this.embeds           = data.embeds;
+    this.attachments      = data.attachments || [];
+    this.embeds           = data.embeds || [];
     this.guild            = client.guilds.get(client.channels.get(this.channelID).guildID)
+    this.channel          = new GuildChannel(client, client.channels.get(this.channelID))
   }
 }
--- Models/TextChannel.js
+++ Models/TextChannel.js
@@ -1,33 +1,58 @@
-const snekfetch = require('snekfetch')
+const {get, put, patch, post} = require('snekfetch')
 class TextChannel {
   constructor(client, data) {
-    this.type = 'text'
+    this.type = data.type;
+    this.guildID = data.guild_id;
+    this.name = data.name;
+    this.position = data.position;
+    this.permissionOverwrites = data.permission_overwrites;
+    this.nsfw = data.nsfw;
+    this.topic = data.topic;
+    this.lastMessageID = this.last_message_id;
+    this.parentMessageID = data.parent_id;
+
+    this.client = client;
+    this.data = data;
   }
 
-  get name() {
-    if (data.name) {
-      return data.name;
-    } else {
-      return 'Unknown';
-    };
-  };
+  // get name() {
+  //   if (this.name) {
+  //     return this.name;
+  //   } else {
+  //     return 'Unknown';
+  //   };
+  // };
 
   send(toSend, options) {
+    let that = this;
     return new Promise(async function(resolve, reject) {
-    let content;
-
+    let payload = {};
     if (!toSend) throw Error('Can\'t send an empty message');
+    if (!options) options = {}
     if (!options.tts) options.tts = false;
-    if (!options.markup) content = toSend;
 
     if (typeof options.markup !== 'undefined' && (typeof options.markup !== 'boolean' || options.markup === true)) {
-        content = `
+        payload.content = `
         \`\`\`${typeof options.markup === true ? '' : options.markup}
           ${toSend}
         \`\`\`
         `
+      } else {
+        payload.content = toSend;
       }
 
-    }
-  });
+    if (typeof options.tts === 'boolean' && options.tts === true) {
+        payload.tts = true;
+      } else {
+        payload.tts = false;
+      }
+    post(`https://discordapp.com/api/v6/channels/${that.data.id}/messages`)
+      .set('Authorization', 'Bot ' + that.client.token)
+      .send(payload)
+      .then(r => resolve(r.body))
+      .catch(e => reject(e))
+    });
+  }
 }
+
+module.exports = TextChannel
