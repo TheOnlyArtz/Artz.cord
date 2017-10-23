80f6012d819edef26a1c9c6f1b7db5e70843da8b
TheOnlyArtz <gamesil456@gmail.com>
2017-10-23 19:23:57 +0300

    A function for creating invite dynamic links

--- Models/TextChannel.js
+++ Models/TextChannel.js
@@ -17,13 +17,28 @@ class TextChannel {
 		this.data = data;
 	}
 
-  // Get name() {
-  //   if (this.name) {
-  //     return this.name;
-  //   } else {
-  //     return 'Unknown';
-  //   };
-  // };
+	createInviteLink(options) {
+		let that = this;
+		return new Promise(async (resolve, reject) => {
+
+			if (!options instanceof Object) {
+					reject('ERROR: The invite options must come in an object.');
+			}
+			try {
+
+				let res = await that.client.APIManager.makeRequest(
+					'post',
+					 that.client.APIManager.endpoints.ENDPOINTS_CHANNELS.invites.create(that.id),
+					 options
+				 );
+
+				resolve(res)
+			} catch (e) {
+				reject(e)
+			}
+
+		});
+	}
 
 }
 
