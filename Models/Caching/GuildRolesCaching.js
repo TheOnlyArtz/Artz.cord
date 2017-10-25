b112f0294c4deb1fdbb419de4032aedee648815d
TheOnlyArtz <gamesil456@gmail.com>
2017-10-25 20:41:28 +0300

    Role caching using new Box

--- Models/Caching/GuildRolesCaching.js
+++ Models/Caching/GuildRolesCaching.js
@@ -1,7 +1,9 @@
+const Box = require('../Box.js');
+
 class GuildRolesCaching {
   constructor(client, data) {
     Object.defineProperty(this, 'client', {value: client});
-    this.roles = new Map();
+    this.roles = new Box();
     data.forEach(role => {
       this.roles.set(role.id, role);
     });
