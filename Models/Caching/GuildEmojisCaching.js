2de1bfa70cb159d04848ce4df2f807f84ba34e2e
TheOnlyArtz <gamesil456@gmail.com>
2017-10-25 20:41:42 +0300

    Emoji caching has new Box

---
+++ Models/Caching/GuildEmojisCaching.js
@@ -0,0 +1,13 @@
+const Box = require('../Box.js');
+class GuildEmojisCaching {
+  constructor(client, data) {
+    Object.defineProperty(this, 'client', {value: client});
+    this.emojis = new Box();
+    data.forEach(emoji => {
+      this.emojis.set(emoji.id, emoji);
+    });
+    return this.emojis;
+  }
+}
+
+module.exports = GuildRolesCaching
