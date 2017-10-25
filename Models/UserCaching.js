e999afeb9c5efc616305349035f730e9b1f5ff4a
TheOnlyArtz <gamesil456@gmail.com>
2017-10-25 16:07:08 +0300

    TODO

--- Models/UserCaching.js
+++ Models/UserCaching.js
@@ -10,6 +10,7 @@ class UserCaching {
 		if (this.iterable && Array.isArray(this.iterable)) {
 			this.iterable.forEach(i => {
 				this.client.users.set(i.user.id, new User(this.client, i.user));
+				// TODO: Make this.members inside guild to return here a new instance of it.
 			});
 		}
 	}
