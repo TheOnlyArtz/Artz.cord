7dd81bb0d2350f1f135176f64f8611c23b09e671
TheOnlyArtz <gamesil456@gmail.com>
2017-12-07 15:09:22 +0200

    Permissions

--- src/Constants.js
+++ src/Constants.js
@@ -12,6 +12,40 @@ class Constants {
 			1: 'ELEVATED'
 		}
 
+		this.PERMISSIONS = {
+			CREATE_INSTANT_INVITE: 1 << 0,
+			KICK_MEMBERS: 1 << 1,
+			BAN_MEMBERS: 1 << 2,
+			ADMINISTRATOR: 1 << 3,
+			MANAGE_CHANNELS: 1 << 4,
+			MANAGE_GUILD: 1 << 5,
+			ADD_REACTIONS: 1 << 6,
+			VIEW_AUDIT_LOG: 1 << 7,
+
+			VIEW_CHANNEL: 1<< 10,
+			SEND_MESSAGE: 1 << 11,
+			SEND_TTS_MESSAGES: 1 << 12,
+			MANAGE_MESSAGES: 1 << 13,
+			EMBED_LINKS: 1 << 14,
+			ATTACH_FILES: 1 << 15,
+			READ_MESSAGE_HISTORY: 1 << 16,
+			MENTIONS_ONLY: 1 << 17,
+			USE_EXTERNAL_EMOJIS: 1 << 18,
+
+			CONNECT: 1 << 20,
+			SPEAK: 1 << 21,
+			MUTE_MEMBERS: 1 << 22,
+			DEAFEN_MEMBERS: 1 << 23,
+			MOVE_MEMBERS: 1 << 24,
+			USE_VAD: 1 << 25,
+
+			CHANGE_NICKNAME: 1 << 26,
+			MANAGE_NICKNAMES: 1 << 27,
+			MANAGE_ROLES: 1 << 28,
+			MANAGE_WEBHOOKS: 1 << 29,
+			MANAGE_EMOJIS: 1 << 30,
+		}
+
 		this.GUILD_VERIFICATION_LEVEL = {
 			0: 'NONE',
 			1: 'VEREFIED_MAIL',
@@ -48,7 +82,10 @@ class Constants {
 			messages: {
 				list: id => `channels/${id}/messages`,
 				get: (ChannelID, MessageID) => `channels/${ChannelID}/messages/${MessageID}`,
-				create: id => `channels/${id}/messages`
+				create: id => `channels/${id}/messages`,
+				delete: (CID, MID) => `channels/${CID}/messages/${MID}`,
+				edit: (CID, MID) => `channels/${CID}/messages/${MID}`,
+				bulkDelete: (CID) => `channels/${CID}/messages/bulk-delete`
 			},
 			reactions: {
 				create: (id, Mid, emoji) => `channels/${id}/messages/${Mid}/${emoji}/@me`,
